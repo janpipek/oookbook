904c54285ac09d59a8615b3c12b3529117c7ac6b
Jan Pipek <jan.pipek@gmail.com>
2013-06-25 04:22:38 +0200

    Adding new books

--- oookbook/models.py
+++ oookbook/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=200)
-    note = models.TextField()
+    note = models.TextField(blank=True, null=True)
 
     def __unicode__(self):
         return "{}: {}".format(self.author, self.title)
--- oookbook/views/books.py
+++ oookbook/views/books.py
@@ -1,12 +1,34 @@
 from django.shortcuts import get_object_or_404, render
+from django.forms import ModelForm
+from django.http import HttpResponseRedirect
 from oookbook.models import Book
 
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'note']
+
 def index(request):
-    books = Book.objects.all().order_by('title')[:5]
-    context = {'books' : books}
-    return render(request, 'books/index.html', context)
+    if request.method == 'GET':
+        books = Book.objects.all().order_by('title')[:20]
+        context = {'books' : books}
+        return render(request, 'books/index.html', context)
+    else:
+        form = BookForm(request.POST)
+        if form.is_valid():
+            book = form.save(commit = False)
+            book.save()
+            return HttpResponseRedirect('/books/')
+        else:
+            return render(request, 'books/new.html', {
+                'form': form,
+            })
 
 def show(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     context = {'book' : book}
     return render(request, 'books/show.html', context)
+
+def new(request):
+    context = { "form" : BookForm() }
+    return render(request, 'books/new.html', context)
