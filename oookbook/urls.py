bcd6e88ad64a034566159fc12efa7e4267859882
Jan Pipek <jan.pipek@gmail.com>
2013-06-25 03:46:41 +0200

    Prepared for book editing page

--- oookbook/urls.py
+++ oookbook/urls.py
@@ -13,7 +13,8 @@ urlpatterns = patterns('',
     # Books
     url(r'^books/?$', views.books.index, name='books.index'),
     url(r'^books/(\d+)/?', views.books.show, name='books.show'),
+    url(r'^books/new', views.books.new, name='books.new'),
 
-    url(r'^users/?$', views.users.index, name='users.index'),
+    url(r'^users/?$', views.users.index, name='users.index'),  
     url(r'^users/(\d+)/?', views.users.show, name='users.show'),
 )
