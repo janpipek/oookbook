2f8c5fa9b0e2bfc55b100fa3ce51a438d2853729
Jan Pipek <jan.pipek@gmail.com>
2013-06-29 16:01:17 +0200

    Introduce Loan and LoanLog classes

--- oookbook/models.py
+++ oookbook/models.py
@@ -6,7 +6,26 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     note = models.TextField(blank=True, null=True)
     user = models.ForeignKey(User)
+    created_at = models.DateTimeField()
 
     def __unicode__(self):
         return u"{}: {}".format(self.author, self.title)
-    
+
+class Loan(models.Model):
+    class Status:
+        REQUESTED = 0
+        APPROVED = 1
+        REJECTED = 2
+        BORROWED = 3
+        RETURNED = 4
+        LOST = 5
+
+    book = models.ForeignKey(Book)
+    user = models.ForeignKey(User)
+    status = models.IntegerField() # TODO: Limit to choices in Status
+
+class LoanLog(models.Model):
+    loan = models.ForeignKey(Loan, db_index = True)
+    user_id = models.ForeignKey(User, db_index = True)
+    status = models.IntegerField()
+    updated_at = models.DateTimeField(db_index = True)
