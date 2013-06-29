3753694706169a7aea32323261b0b00662721d2e
Jan Pipek <jan.pipek@gmail.com>
2013-06-29 17:41:49 +0200

    loans.borrow view

--- oookbook/views/loans.py
+++ oookbook/views/loans.py
@@ -1,2 +1,17 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+
+from oookbook.models import Book, Loan, LoanLog
+
+@login_required
 def borrow(request, book_id):
-    pass
+    book = get_object_or_404(Book, pk=book_id)
+    loan = Loan(book = book, user= request.user, status = Loan.Status.REQUESTED )
+    loan.save()
+    try:
+        loan.log()
+    except:
+        loan.delete()
+        raise
+    return HttpResponseRedirect('/books/')
