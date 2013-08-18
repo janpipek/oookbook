82d96f08a702044049a123fa86b4c5912c3882b3
Jan Pipek <jan.pipek@gmail.com>
2013-08-18 23:34:49 +0200

    Enter and view loans

--- oookbook/urls.py
+++ oookbook/urls.py
@@ -19,9 +19,14 @@ urlpatterns = patterns('',
     # Books
     url(r'^books/$', views.books.index, name='books.index'),
     url(r'^books/(\d+)/$', views.books.show, name='books.show'),
-    url(r'^books/(?P<book_id>\d+)/borrow/$', views.loans.borrow, name='books.borrow'),
+    # url(r'^books/(?P<book_id>\d+)/borrow/$', views.loans.borrow, name='books.borrow'),
     url(r'^books/new/$', views.books.new, name='books.new'),
 
+    # Loans
+    url(r'^loans/$', views.loans.index, name='loans.index'),
+    url(r'^loans/new/$', views.loans.new, name='loans.new'),
+
+    # Users
     url(r'^users/$', views.users.index, name='users.index'),  
     url(r'^users/(\d+)/$', views.users.show, name='users.show'),
 )
--- oookbook/views/loans.py
+++ oookbook/views/loans.py
@@ -1,17 +1,38 @@
+from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 
 from oookbook.models import Book, Loan, LoanLog
 
 @login_required
-def borrow(request, book_id):
+def new(request):
+    book_id = request.REQUEST.get("book_id", None)
     book = get_object_or_404(Book, pk=book_id)
     loan = Loan(book = book, user= request.user, status = Loan.Status.REQUESTED )
-    loan.save()
-    try:
-        loan.log()
-    except:
-        loan.delete()
-        raise
-    return HttpResponseRedirect('/books/')
+
+    return render(request, 'loans/new.html', {
+        'book' : book,
+        'loan' : loan,
+    })
+
+def index(request):
+    if request.method == 'GET':
+
+        loans = Loan.objects.filter(user_id = request.user.id)
+        return render(request, 'loans/index.html', {
+            'loans' : loans
+        })
+    else:
+        book_id = request.REQUEST.get("book_id", None)
+        book = get_object_or_404(Book, pk=book_id)
+        loan = Loan(book = book, user = request.user, status = Loan.Status.REQUESTED )
+        loan.save()
+        try:
+            loan.log()
+        except:
+            loan.delete()
+            raise
+        # Create a new Loan
+        return HttpResponseRedirect('/loans/')
